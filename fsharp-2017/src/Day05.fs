0f73a2011b950096b000fcb32c79b401b05098cd
John Leung <john.leung@cancerlinq.org>
2019-02-28 23:37:04 -0800

    Day 5 complete

--- fsharp-2017/src/Day05.fs
+++ fsharp-2017/src/Day05.fs
@@ -7,14 +7,19 @@ let step (a:int[], i:int) =
   a.[i] <- a.[i] + 1
   i'
 
-let doSteps (a:int[]) = 
-  let mutable i = step (a, 0)
+let step2 (a:int[], i:int) = 
+  let i' = i + a.[i]
+  a.[i] <- a.[i] + (if a.[i] > 2 then - 1 else 1)
+  i'
+
+let doSteps (a:int[]) stepper = 
+  let mutable i = stepper (a, 0)
   let mutable steps = 1
   while i < Array.length a do
-    i <- step (a, i)
+    i <- stepper (a, i)
     steps <- steps + 1
   steps
 
 let run (instr:string) =
-  let a = instr |> mapSplitLine int
-  printfn "Day 5-1 answer: %A" <| doSteps a
+  printfn "Day 5-1 answer: %A" <| doSteps (instr |> mapSplitLine int) step
+  printfn "Day 5-2 answer: %A" <| doSteps (instr |> mapSplitLine int) step2
