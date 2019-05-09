5d06d2f6cb2e2a00597c14983b0488ed91d5fe66
John Leung <john.leung@cancerlinq.org>
2019-05-09 10:51:23 -0700

    Day 6 complete

---
+++ js-pipe-fkit_2017/src/day06.js
@@ -0,0 +1,45 @@
+const { maximum } = require('fkit')
+const { len, strToInts } = require('./helpers')
+
+const zeroAt = idx => a /*int,[int]*/ => a.map((x, i) => i==idx? 0: x)
+const mapAdd = n => a /*int,[int]*/ => a.map(x => x + n)
+
+/// Add 1 to cnt elms in a, starting from elm __after__ idx 
+const addOne = (idx, cnt) => a /*int,int,[int]*/ =>
+  cnt == 0? a :
+  a.map((x, i) => {
+    const toEnd = a.length - idx - 1
+    const _01 = (i > idx && i-idx <= cnt) || (i < idx && i+toEnd < cnt)? 1 : 0
+    return x + _01
+  })
+
+const hash = a => `${a}`
+const setHash = (hist, val, a) => {
+  hist[hash(a)] = val
+  return hist
+}
+
+const alloc = a /*[int]*/ => {
+  const max = maximum(a)
+  const i = a.findIndex(x => x == max)
+  return a
+  |> zeroAt(i)
+  |> mapAdd(~~(max / a.length))
+  |> addOne(i, max % a.length)
+}
+
+/// returns Number of allocations needed for array a
+const cntAllocs = (a, hist) /*[int],{}*/ =>
+  hist[hash(a)]? len(hist): cntAllocs(alloc(a), setHash(hist, true, a))
+
+/// return Number of allocations between first time array a is repeated
+const cntPrvSeen = (a, hist) /*[int],{}*/ => {
+  const i = hist[hash(a)]
+  return i != null? len(hist)-i: cntPrvSeen(alloc(a), setHash(hist, len(hist), a))
+}
+
+module.exports = (inStr, log) => {
+  const ints = strToInts(inStr, '\t')
+  log.p1( cntAllocs(ints, {}) )
+  log.p2( cntPrvSeen(ints, {}) )
+}
--- js-pipe-fkit_2017/src/helpers.js
+++ js-pipe-fkit_2017/src/helpers.js
@@ -3,16 +3,19 @@ const { map } = require('fkit')
 const strToWords2D = s =>
   s.split('\n') |> map(s => s.split(' '))
 
-const strToInts2D = s =>
+const strToInts2D = (s, sep='\t') =>
   s.split('\n')
-  |> map(s => s.split('\t') |> map(Number))
+  |> map(s => s.split(sep) |> map(Number))
 
 const charsStrToInts = s => s.split('') |> map(Number)
-const strToInts = s => s.split('\n') |> map(Number)
+const strToInts = (s, sep='\n') => s.split(sep) |> map(Number)
+
+const len = o => Object.keys(o).length
 
 module.exports = {
   strToInts2D,
   strToWords2D,
   charsStrToInts,
   strToInts,
+  len,
 }
