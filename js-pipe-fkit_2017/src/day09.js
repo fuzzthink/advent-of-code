3cbd8b47cad1d9045bf772c03e638af63d5017eb
John Leung <john.leung@cancerlinq.org>
2019-05-19 00:09:48 -0700

    Day 9 complelet

--- js-pipe-fkit_2017/src/day09.js
+++ js-pipe-fkit_2017/src/day09.js
@@ -1,28 +1,39 @@
-const { filter, sort, id } = require('fkit')
-const { strToWords2D } = require('./helpers')
-
-const parse = s => s
+/**
+ * @return {String} of nested {}
+ */
+const getGroupStr = s => s
   .replace(/!./g, '')      // ignore ! and any char after
   .replace(/<[^>]*>/g, '') // < > are comments, ignore all chars in it but >
   .replace(/,/g, '')       // , is a separator of comments or groups
-  
-const countGroups = parsed /*str*/ => parsed.length / 2
+ 
+const countGroups = groupStr => groupStr.length / 2
 
-const getScore = parsed /*str*/ => {
+const getScore = groupStr => { // See 09.md
   let lv = 0
-  return parsed.split('').reduce((acc, c) => {
+  return groupStr.split('').reduce((acc, c) => {
     const delta = c=='{'? 1: -1
     lv += delta
     return delta==1? lv + acc: acc
   }, 0)
 }
 
-const run = (inStr, log) => {
-  log.p1( parse(inStr) |> getScore )
+/**
+ * @return {Number} of non-!x chars in <>
+ */
+const charsInComment = str => {
+  const s = str.replace(/!./g, '')
+  return s.length - s.replace(/<[^>]*>/g, '<>').length
 }
+
+const run = (inStr, log) => {
+  log.p1( getGroupStr(inStr) |> getScore )
+  log.p2( charsInComment(inStr) )
+}
+
 module.exports = {
   run,
-  parse,
+  getGroupStr,
   countGroups,
   getScore,
+  charsInComment,
 }
--- js-pipe-fkit_2017/test/09.js
+++ js-pipe-fkit_2017/test/09.js
@@ -1,5 +1,5 @@
 const should = require('should')
-const { parse, countGroups, getScore } = require('../dist/day09')
+const { getGroupStr, countGroups, getScore, charsInComment } = require('../dist/day09')
 
 // Test examples from instruction
 const counts = {
@@ -38,18 +38,41 @@ const scores = {
   expected: [ 1, 6, 5, 16, 1, 9, 9, 3 ] 
 }
 
+const charsRemoved = {
+  data:
+`<>
+<random characters>
+<<<<>
+<{!>}>
+<!!>
+<!!!>>
+<{o"i!a,<{i<a>`.split('\n'),
+  expected: [0, 17, 3, 2, 0, 0, 10 ]
+}
+
 describe('countGroups()', () => {
   counts.data.map((s, i) => {
     const expected = counts.expected[i]
     it(`of ${s} = ${expected}`, () => {
-      countGroups(parse(s)).should.equal(expected)
+      countGroups(getGroupStr(s)).should.equal(expected)
     })
   })
+})
 
+describe('getScore()', () => {
   scores.data.map((s, i) => {
     const expected = scores.expected[i]
     it(`of ${s} = ${expected}`, () => {
-      getScore(parse(s)).should.equal(expected)
+      getScore(getGroupStr(s)).should.equal(expected)
+    })
+  })
+})
+
+describe('countGroups()', () => {
+  charsRemoved.data.map((s, i) => {
+    const expected = charsRemoved.expected[i]
+    it(`of ${s} = ${expected}`, () => {
+      charsInComment(s).should.equal(expected)
     })
   })
 })
