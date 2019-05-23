261bfcdc292eb0ef57804c5b4da8b8c792bc9b5c
John Leung <john.leung@cancerlinq.org>
2019-05-22 23:45:51 -0700

    Day 10 complete

--- js-pipe-fkit_2017/src/day10.js
+++ js-pipe-fkit_2017/src/day10.js
@@ -1,49 +1,60 @@
-const { reverse, concat, map } = require('fkit')
+const { reverse, map } = require('fkit')
 
 /**
  * @return {[int]} of a[frm..frm+cnt] reversed
  */
 const reverseAt = (frm, cnt, a) /*int,int,[int]*/ => 
   (frm+cnt > a.length
-    ? concat(a.slice(frm), a.slice(0, frm+cnt - a.length))
+    ? a.slice(frm).concat( a.slice(0, frm+cnt - a.length) )
     : a.slice(frm, frm+cnt)
   ) |> reverse
 
-/** See instructions/10.md
- */
-const knotHash = (list, lens, iList=0, skip=0) => {
+const genSparseHash = (lens, list, iList=0, skip=0, returnMeta=false) => {
+  list = list || [...Array(256).keys()]
   if (lens.length == 0)
-    return list
+    return returnMeta? [list, iList, skip] : list
   else {
     const reversed = reverseAt(iList, lens[0], list)
+    const revLen0 = list.length - iList
     const wrapped = iList + lens[0] > list.length
-    let _list
-    if (wrapped) {
-      const revLen0 = list.length - iList
-      const revLen1 = reversed.length - revLen0
-      _list = concat(
-        reversed.slice(revLen0),
-        list.slice(revLen1, revLen1 + list.length - reversed.length),
-        reversed.slice(0, revLen0),
-      )
-    } else {
-      _list = concat(
-        list.slice(0, iList),
-        reversed,
-        list.slice(iList + reversed.length),
-      )
-    } 
-    iList += lens[0] + skip
-    return knotHash(_list, lens.slice(1), iList % list.length, skip + 1)
+    list = wrapped? [
+      ...reversed.slice(revLen0),
+      ...list.slice(reversed.length - revLen0, list.length - revLen0),
+      ...reversed.slice(0, revLen0),
+    ] : [
+      ...list.slice(0, iList),
+      ...reversed,
+      ...list.slice(iList + reversed.length),
+    ] 
+    iList = (iList + lens[0] + skip) % list.length
+    return genSparseHash(lens.slice(1), list, iList, skip + 1, returnMeta)
   }
+}
+
+const mapASCII = s => [...s].map(s => s.charCodeAt())
+
+const knotHash = (lensStr, list, iList=0, skip=0, rounds=64) => {
+  let lens = [...mapASCII(lensStr), 17, 31, 73, 47, 23 ]
+  list = list || [...Array(256).keys()]
+  for (let i=0; i <rounds; i++) {
+    [list, iList, skip] = genSparseHash(lens, list, iList, skip, true)
+  }
+  return [...Array(16).keys()]
+    .map(i => list.slice(i*16, i*16 + 16)
+      .reduce((a,c) => a^c , 0)
+    )
+    .map(n => n.toString(16).padStart(2, '0'))
+    .join('')
 }
 
 const run = (inStr, log) => {
   const lens = inStr.split(',') |> map(Number)
-  const hash = knotHash( Array.from({length: 256}, (v, i) => i), lens )
-  log.p1( hash[0] * hash[1] )
+  const shash = genSparseHash(lens)
+  log.p1(shash[0] * shash[1]) // 7888
+  log.p2(knotHash(inStr)) // decdf7d377879877173b7f2fb131cf1b
 }
 module.exports = { 
   run,
+  genSparseHash,
   knotHash,
 }
--- js-pipe-fkit_2017/test/10.js
+++ js-pipe-fkit_2017/test/10.js
@@ -1,15 +1,26 @@
 const should = require('should')
-const { knotHash } = require('../dist/day10')
+const { genSparseHash, knotHash } = require('../dist/day10')
 
 // Test examples from instruction
-const data = {
+const p1 = {
   list: [0, 1, 2, 3, 4], 
   lens: [3, 4, 1, 5],
   expected: [3, 4, 2, 1, 0]
 }
+const p2 = [
+  { input: '', expected: 'a2582a3a0e66e6e86e3812dcb672a272' },
+  { input: 'AoC 2017', expected: '33efeb34ea91902bb2f59c9920caa6cd' },
+  { input: '1,2,3', expected: '3efbe78a8d82f29979031a4aa0b16a9d' },
+  { input: '1,2,4', expected: '63960835bcdc130f0b66d7ff4f6a5a8e' },
+]
 
-describe('knotHash()', () => {
-  it(`of ${data.list} = ${data.expected}`, () => {
-    knotHash(data.list, data.lens, 0).should.deepEqual(data.expected)
+describe('Day 10', () => {
+  it(`sparseHash of [${p1.lens}] with list ${p1.list}`, () => {
+    genSparseHash(p1.lens, p1.list).should.deepEqual(p1.expected)
+  })
+  p2.map(d => {
+    it(`knoxHash of "${d.input}" with list 0..255`, () => {
+      knotHash(d.input).should.equal(d.expected)
+    })
   })
 })
